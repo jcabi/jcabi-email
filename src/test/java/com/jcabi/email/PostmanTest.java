7c379356fba36c0fa80186c7e5efef99f6b31e6a
Dmitri Pisarenko <dp@altruix.co>
2015-01-08 17:20:04 +0300

    Fixed code quality issues.

--- src/test/java/com/jcabi/email/PostmanTest.java
+++ src/test/java/com/jcabi/email/PostmanTest.java
@@ -1,20 +1,32 @@
-/*
- * This file is part of Tprot.
+/**
+ * Copyright (c) 2014, jcabi.com
+ * All rights reserved.
  *
- * Tprot is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met: 1) Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer. 2) Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution. 3) Neither the name of the jcabi.com nor
+ * the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
  *
- * Tprot is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Tprot.  If not, see <http://www.gnu.org/licenses/>.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package com.jcabi.email;
 
 import com.google.common.base.Joiner;
@@ -23,10 +35,8 @@ import com.jcabi.email.stamp.StRecipient;
 import com.jcabi.email.stamp.StSender;
 import com.jcabi.email.stamp.StSubject;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import javax.mail.Address;
 import javax.mail.Message;
-import javax.mail.MessagingException;
 import javax.mail.Transport;
 import javax.mail.internet.MimeMessage;
 import org.junit.Assert;
@@ -35,12 +45,17 @@ import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
-public class PostmanTest {
+/**
+ * Test class for Postman class.
+ */
+public final class PostmanTest {
     /**
-     * Test for issue #8.
+     * Test for issue #8 (unicode/UTF-8 is broken).
+     * @throws Exception Thrown in case of problem of writing a message to
+     *  string.
      */
     @Test
-    public void utf8Handling() throws MessagingException, IOException {
+    public final void utf8Handling() throws Exception {
         final Transport transport = Mockito.mock(Transport.class);
         final Wire wire = Mockito.mock(Wire.class);
         Mockito.when(wire.connect()).thenReturn(transport);
